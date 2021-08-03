0fa65f43fa39e6a5b3a7252acbacc1ad15fd241d
papercrane1001 <papercrane1001@gmail.com>
2021-08-03 10:20:51 -0700

    Got started, at least.  Need to decide if override or new method.

--- Source/ShapedCharges/ShapedCharges/Class1.cs
+++ Source/ShapedCharges/ShapedCharges/Class1.cs
@@ -6,11 +6,28 @@ using System.Threading.Tasks;
 
 using Verse;
 using RimWorld;
+using UnityEngine;
 
 namespace ShapedCharges
 {
     public class DirectedExplosion : Explosion
     {
 
+    }
+
+    public class DirectedExplosion_DamageWorker : DamageWorker
+    {
+        public override IEnumerable<IntVec3> ExplosionCellsToHit(IntVec3 center, Map map, float radius, 
+            IntVec3? needLOSToCell1 = null, IntVec3? needLOSToCell2 = null)
+        {
+            //return base.ExplosionCellsToHit(center, map, radius, needLOSToCell1, needLOSToCell2);
+            //DamageWorker.openCells.Clear();
+            List<IntVec3> myOpenCells = new List<IntVec3>(); //note: openCells is private
+            List<IntVec3> myadjWallCells = new List<IntVec3>();
+
+            int num = GenRadial.NumCellsInRadius(radius);
+
+
+        }
     }
 }
