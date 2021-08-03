761ea56011cf73d6bc8142964038e50221774f4b
papercrane1001 <papercrane1001@gmail.com>
2021-08-03 15:49:30 -0700

    I've got something that might work.  Need to get it to the point I can test it.

--- Source/ShapedCharges/ShapedCharges/ShapedChargesClasses.cs
+++ Source/ShapedCharges/ShapedCharges/ShapedChargesClasses.cs
@@ -12,10 +12,167 @@ namespace ShapedCharges
 {
     public class Projectile_DirectedExplosive : Projectile_Explosive
     {
-        protected override void Explode()
+        protected virtual void Explode(IntVec3 vector)
         {
             //base.Explode();
+            Map map = base.Map;
+            this.Destroy(DestroyMode.Vanish);
+            if (this.def.projectile.explosionEffect != null)
+            {
+                Effecter effecter = this.def.projectile.explosionEffect.Spawn();
+                effecter.Trigger(new TargetInfo(base.Position, map, false), new TargetInfo(base.Position, map, false));
+                effecter.Cleanup();
+            }
+            IntVec3 position = base.Position;
+            Map map2 = map;
+            float explosionRadius = this.def.projectile.explosionRadius;
+            DamageDef damageDef = this.def.projectile.damageDef;
+            Thing launcher = this.launcher;
+            int damageAmount = base.DamageAmount;
+            float armorPenetration = base.ArmorPenetration;
+            SoundDef soundExplode = this.def.projectile.soundExplode;
+            ThingDef equipmentDef = this.equipmentDef;
+            ThingDef def = this.def;
+            Thing thing = this.intendedTarget.Thing;
+            ThingDef postExplosionSpawnThingDef = this.def.projectile.postExplosionSpawnThingDef;
+            float postExplosionSpawnChance = this.def.projectile.postExplosionSpawnChance;
+            int postExplosionSpawnThingCount = this.def.projectile.postExplosionSpawnThingCount;
+            ThingDef preExplosionSpawnThingDef = this.def.projectile.preExplosionSpawnThingDef;
+            float preExplosionSpawnChance = this.def.projectile.preExplosionSpawnChance;
+            int preExplosionSpawnThingCount = this.def.projectile.preExplosionSpawnThingCount;
+            GenDirectedExplosion.DoExplosion(vector,
+                position, map2, explosionRadius, damageDef, launcher, damageAmount, armorPenetration, soundExplode, equipmentDef, 
+                def, thing, postExplosionSpawnThingDef, postExplosionSpawnChance, postExplosionSpawnThingCount, 
+                this.def.projectile.applyDamageToExplosionCellsNeighbors, preExplosionSpawnThingDef, preExplosionSpawnChance, 
+                preExplosionSpawnThingCount, this.def.projectile.explosionChanceToStartFire, this.def.projectile.explosionDamageFalloff, 
+                new float?(this.origin.AngleToFlat(this.destination)), null);
+        }
+    }
 
+    public class GenDirectedExplosion
+    {
+        public static void DoExplosion(IntVec3 vector, IntVec3 center, Map map, float radius, DamageDef damType, Thing instigator, int damAmount = -1, 
+            float armorPenetration = -1f, SoundDef explosionSound = null, ThingDef weapon = null, ThingDef projectile = null, 
+            Thing intendedTarget = null, ThingDef postExplosionSpawnThingDef = null, float postExplosionSpawnChance = 0f, 
+            int postExplosionSpawnThingCount = 1, bool applyDamageToExplosionCellsNeighbors = false, 
+            ThingDef preExplosionSpawnThingDef = null, float preExplosionSpawnChance = 0f, int preExplosionSpawnThingCount = 1, 
+            float chanceToStartFire = 0f, bool damageFalloff = false, float? direction = null, List<Thing> ignoredThings = null)
+        {
+            if (map == null)
+            {
+                Log.Warning("Tried to do explosion in a null map.");
+                return;
+            }
+            if (damAmount < 0)
+            {
+                damAmount = damType.defaultDamage;
+                armorPenetration = damType.defaultArmorPenetration;
+                if (damAmount < 0)
+                {
+                    Log.ErrorOnce("Attempted to trigger an explosion without defined damage", 91094882);
+                    damAmount = 1;
+                }
+            }
+            if (armorPenetration < 0f)
+            {
+                armorPenetration = (float)damAmount * 0.015f;
+            }
+
+            MyExplosion explosion = (MyExplosion)GenSpawn.Spawn(ThingDefOf.Explosion, center, map, WipeMode.Vanish);
+            //MyExplosion myExplosion = ()
+            
+            IntVec3? needLOSToCell = null;
+            IntVec3? needLOSToCell2 = null;
+            if (direction != null)
+            {
+                CalculateNeededLOSToCells(center, map, direction.Value, out needLOSToCell, out needLOSToCell2);
+            }
+            explosion.radius = radius;
+            explosion.damType = damType;
+            explosion.instigator = instigator;
+            explosion.damAmount = damAmount;
+            explosion.armorPenetration = armorPenetration;
+            explosion.weapon = weapon;
+            explosion.projectile = projectile;
+            explosion.intendedTarget = intendedTarget;
+            explosion.preExplosionSpawnThingDef = preExplosionSpawnThingDef;
+            explosion.preExplosionSpawnChance = preExplosionSpawnChance;
+            explosion.preExplosionSpawnThingCount = preExplosionSpawnThingCount;
+            explosion.postExplosionSpawnThingDef = postExplosionSpawnThingDef;
+            explosion.postExplosionSpawnChance = postExplosionSpawnChance;
+            explosion.postExplosionSpawnThingCount = postExplosionSpawnThingCount;
+            explosion.applyDamageToExplosionCellsNeighbors = applyDamageToExplosionCellsNeighbors;
+            explosion.chanceToStartFire = chanceToStartFire;
+            explosion.damageFalloff = damageFalloff;
+            explosion.needLOSToCell1 = needLOSToCell;
+            explosion.needLOSToCell2 = needLOSToCell2;
+            explosion.StartExplosion(vector, explosionSound, ignoredThings);
+        }
+
+        public static void CalculateNeededLOSToCells(IntVec3 position, Map map, float direction, out IntVec3? needLOSToCell1, out IntVec3? needLOSToCell2)
+        {
+            needLOSToCell1 = null;
+            needLOSToCell2 = null;
+            if (position.CanBeSeenOverFast(map))
+            {
+                return;
+            }
+            direction = GenMath.PositiveMod(direction, 360f);
+            IntVec3 intVec = position;
+            intVec.z++;
+            IntVec3 intVec2 = position;
+            intVec2.z--;
+            IntVec3 intVec3 = position;
+            intVec3.x--;
+            IntVec3 intVec4 = position;
+            intVec4.x++;
+            if (direction < 90f)
+            {
+                if (intVec3.InBounds(map) && intVec3.CanBeSeenOverFast(map))
+                {
+                    needLOSToCell1 = new IntVec3?(intVec3);
+                }
+                if (intVec.InBounds(map) && intVec.CanBeSeenOverFast(map))
+                {
+                    needLOSToCell2 = new IntVec3?(intVec);
+                    return;
+                }
+            }
+            else if (direction < 180f)
+            {
+                if (intVec.InBounds(map) && intVec.CanBeSeenOverFast(map))
+                {
+                    needLOSToCell1 = new IntVec3?(intVec);
+                }
+                if (intVec4.InBounds(map) && intVec4.CanBeSeenOverFast(map))
+                {
+                    needLOSToCell2 = new IntVec3?(intVec4);
+                    return;
+                }
+            }
+            else if (direction < 270f)
+            {
+                if (intVec4.InBounds(map) && intVec4.CanBeSeenOverFast(map))
+                {
+                    needLOSToCell1 = new IntVec3?(intVec4);
+                }
+                if (intVec2.InBounds(map) && intVec2.CanBeSeenOverFast(map))
+                {
+                    needLOSToCell2 = new IntVec3?(intVec2);
+                    return;
+                }
+            }
+            else
+            {
+                if (intVec2.InBounds(map) && intVec2.CanBeSeenOverFast(map))
+                {
+                    needLOSToCell1 = new IntVec3?(intVec2);
+                }
+                if (intVec3.InBounds(map) && intVec3.CanBeSeenOverFast(map))
+                {
+                    needLOSToCell2 = new IntVec3?(intVec3);
+                }
+            }
         }
     }
 
