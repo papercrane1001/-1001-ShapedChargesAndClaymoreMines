f0216482728b9fdc564c573aed374dd1155a6e25
papercrane1001 <papercrane1001@gmail.com>
2021-08-05 12:03:29 -0700

    Made a tripwire.  Tripwire isn't yet attached to the claymore.  Also, the explosion does no damage at all.

---
+++ Source/ShapedCharges/ShapedCharges/CompProperties_TripFuse.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace ShapedCharges
+{
+    public class CompProperties_TripFuse : CompProperties
+    {
+        public CompProperties_TripFuse()
+        {
+            this.compClass = typeof(CompTripFuse);
+        }
+
+        public override IEnumerable<string> ConfigErrors(ThingDef parentDef)
+        {
+			foreach (string text in base.ConfigErrors(parentDef))
+			{
+				yield return text;
+			}
+			IEnumerator<string> enumerator = null;
+			if (parentDef.tickerType != TickerType.Normal)
+			{
+				yield return string.Concat(new object[]
+				{
+					"CompTripFuse needs tickerType ",
+					TickerType.Rare,
+					" or faster, has ",
+					parentDef.tickerType
+				});
+			}
+			if (parentDef.CompDefFor<CompDirectedExplosive>() == null)
+			{
+				yield return "CompTripFuse requires a CompDirectedExplosive";
+			}
+			yield break;
+			yield break;
+		}
+
+		public ThingDef target;
+
+		// Token: 0x0400223F RID: 8767
+		public float radius;
+	}
+}
---
+++ Source/ShapedCharges/ShapedCharges/CompTripFuse.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace ShapedCharges
+{
+    public class CompTripFuse : ThingComp
+    {
+        public CompProperties_TripFuse Props
+        {
+            get
+            {
+                return (CompProperties_TripFuse)this.props;
+            }
+        }
+
+        public override void CompTick()
+        {
+            //base.CompTick();
+            if(Find.TickManager.TicksGame % 250 == 0)
+            {
+                this.CompTickRare();
+            }
+        }
+
+        public override void CompTickRare()
+        {
+            //Check if anything is in front of this.parent.Position using this.parent.Rotation.AsVector2
+            //iv2.GetEdifice(map) != null
+            //iv2.GetFistPawn(map) != null
+            IntVec3 vec = this.parent.Position;
+
+            IntVec3 orientation = new IntVec3((int)this.parent.Rotation.AsVector2.x, 0, (int)this.parent.Rotation.AsVector2.y);
+            vec += orientation;
+            while(vec.GetEdifice(this.parent.Map) == null)
+            {
+                if(vec.GetFirstPawn(this.parent.Map) != null)
+                {
+                    ((CompDirectedExplosive)this.parent.GetComp<CompDirectedExplosive>()).Detonaate(orientation, this.parent.Map);
+                }
+            }
+            
+
+            
+
+        }
+    }
+}
